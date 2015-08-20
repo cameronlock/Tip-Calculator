4b1819642048f28d5769b74ff8bd8a0f28013790
Cameron Lock <cameron.lock@gmail.com>
2015-08-20 15:31:38 -0700

    Tip Calculator
    Basic tip calculator, an assignment for Codepath’s iOS for Designers
    course.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Tip Calculator by Cameron Lock
+
+This is a simple tip calculator for application to Codepath's iOS for Designers Course. I've restyled the app to be more colorful and inviting. The user can choose from three tip percentages, easily identified by emoticons that represent quality of service.
+
+Time spent: 5 hours spent in total
+
+Completed user stories:
+
+ * [x] Required: User can calculate an 18%, 20% or 22% tip to the second decimal place
+ * [x] Optional: Splitting the bill feature
+ * [x] Optional: The tip animates when the value changes
+ * [x] Optional: Custom fonts, colors and imagery
+ * [x] Optional: Custom launch and app icons
+ 
+Notes:
+
+I added a bill split function that I wrote on my own based off of the logic from the other features.
+
+Walkthrough of all user stories:
+
+![Video Walkthrough](walkthrough.gif)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+Credits:
+[Avenir](http://www.linotype.com/1245613/avenir-family.html?PHPSESSID=d595d82bc6aad82d8c80a130a13f4711) Font by [Linotype](http://www.linotype.com).
+Fork icon made by [Freepik](http://www.flaticon.com/authors/freepik) from [Flaticon](www.flaticon.com).
--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var twosplitLabel: UILabel!
+    
+    @IBOutlet weak var threesplitLabel: UILabel!
+    
+    @IBOutlet weak var foursplitLabel: UILabel!
+    
+    @IBOutlet weak var fivesplitLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        var twosplit = total / 2
+        var threesplit = total / 3
+        var foursplit = total / 4
+        var fivesplit = total / 5
+        
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+
+        tipLabel.text = updateTipValue(tip)
+        totalLabel.text = String(format: "$%.2f", total)
+
+        
+        twosplitLabel.text = String(format: "$%.2f", twosplit)
+        threesplitLabel.text = String(format: "$%.2f", threesplit)
+        foursplitLabel.text = String(format: "$%.2f", foursplit)
+        fivesplitLabel.text = String(format: "$%.2f", fivesplit)
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing (true)
+    }
+
+
+    
+    
+    func updateTipValue(tip: Double) -> String {
+        
+        UIView.animateWithDuration(0.1 ,
+            animations: {
+                self.tipLabel.transform = CGAffineTransformMakeScale(1.3, 1.3)
+            },
+            
+            completion: { finish in
+                UIView.animateWithDuration(0.1){
+                    self.tipLabel.transform = CGAffineTransformIdentity
+                }
+        })
+        
+        
+        return String(format: "$%.2f", tip)
+    }
 
 }
 
